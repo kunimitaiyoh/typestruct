8fb1179b68f28a8cdc58431aa449ff026ffd2000
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-09-02 14:28:16 +0900

    add rules to tslint.json.

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,3 @@
-import { Decoder, object } from "type-safe-json-decoder";
+import { Decoder } from "type-safe-json-decoder";
 
 export declare function generateDecoder<T extends object>(): Decoder<T>;
