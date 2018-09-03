3e78669f7b9c8ad4aafe91f67587e1b212c86f43
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-09-03 10:16:24 +0900

    implementing transformer.

--- lib/transformer.ts
+++ lib/transformer.ts
@@ -3,6 +3,7 @@ import * as ts from "typescript";
 import { Node, Program, SourceFile, TransformationContext, TransformerFactory, visitEachChild } from "typescript";
 
 const index = path.join(__dirname, "index.ts");
+const FUNCTION_NAME = "generateDecoder";
 
 /**
  * @see https://github.com/kimamula/ts-transformer-keys/blob/master/transformer.ts
@@ -22,5 +23,36 @@ function visitNodeAndChildren(node: Node, program: Program, context: Transformat
 }
 
 function visitNode(node: ts.Node, program: ts.Program): Node {
+  const typeChecker = program.getTypeChecker();
+  if (!isDecoderGeneration(node, typeChecker)) {
+    return node;
+  }
+  if (!node.typeArguments) {
+    throw new Error("type argument is required.");
+  }
+  const type = typeChecker.getTypeFromTypeNode(node.typeArguments[0]);
+  return buildDecoder(type, typeChecker);
+}
+
+function isDecoderGeneration(node: ts.Node, typeChecker: ts.TypeChecker): node is ts.CallExpression {
+  if (node.kind !== ts.SyntaxKind.CallExpression) {
+    return false;
+  }
+  const signature = typeChecker.getResolvedSignature(node as ts.CallExpression);
+  if (typeof signature === "undefined") {
+    return false;
+  }
+
+  if (signature.declaration !== undefined && signature.declaration.kind === ts.SyntaxKind.CallSignature) {
+    const declaration: ts.CallSignatureDeclaration = signature.declaration;
+    return path.join(declaration.getSourceFile().fileName) === index
+      && declaration.name !== undefined
+      && declaration.name.getText() === FUNCTION_NAME;
+  } else {
+    return false;
+  }
+}
+
+function buildDecoder(type: ts.Type, typeChecker: ts.TypeChecker): ts.CallExpression {
   throw new Error("not implemented!");
 }
